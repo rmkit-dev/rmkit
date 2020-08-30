7050bff09117c7775535596955abc923c23b44f5
okay <okay@chalk>
2020-08-30 08:56:26 -0700

    [build] add workflow that uploads builds to external server

--- scripts/build/docker_release.sh
+++ scripts/build/docker_release.sh
@@ -2,9 +2,9 @@ OUTDIR=artifacts
 mkdir ${OUTDIR}
 docker run -i --rm -v "${PWD}/${OUTDIR}:/mnt/artifacts" rmharmony /bin/bash << COMMANDS
 mkdir -p src/build
-make rmkit.h
-make bundle
+make
 cp -r src/build/* /mnt/artifacts
+sha256sum src/build/*.exe > /mnt/artifacts/sha256sum.txt
 ls -la src/build/ > /mnt/artifacts/ls.txt
 chown -R $(id -u):$(id -u) /mnt/artifacts
 COMMANDS
