rmkit-dev/rmkit

commit 154af55c2d40a371e6d9447070976b937fc3a5eb

[proc] rename proc::ls -> proc::list_procs

src/shared/proc.cpy
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <unordered_set>
+#include <thread>
 #define PID 1
 #define PGROUP 4
 
@@ -119,7 +120,7 @@ namespace proc:
 
     return found
 
-  vector<Proc> ls(vector<string> &args):
+  vector<Proc> list_procs(vector<string> &args):
     vector<Proc> ret;
 
     needle := string("")
@@ -152,7 +153,7 @@ namespace proc:
     return ret
 
   void groupkill(int signal, vector<string> &args):
-    procs := ls(args)
+    procs := list_procs(args)
     unordered_set<string> tokill
     for auto proc : procs:
       tokill.insert(proc.stat[PGROUP])
@@ -164,7 +165,7 @@ namespace proc:
 
   bool is_running(string bin):
     vector<string> bins = { bin }
-    procs := ls(bins)
+    procs := list_procs(bins)
     if procs.size() == 0:
       return false
 
@@ -188,7 +189,7 @@ namespace proc:
     return ret
 
   map<string, bool> is_running(vector<string> bins):
-    procs := ls(bins)
+    procs := list_procs(bins)
     return is_running(bins, procs)
 
   MemInfo read_mem_total():
@@ -319,7 +320,7 @@ namespace proc:
 
   bool check_process(string name):
     args := vector<string>{name}
-    procs := ls(args)
+    procs := list_procs(args)
     debug "CHECKING PROCESS", name, procs.size()
     return procs.size() > 0
 
@@ -335,8 +336,11 @@ namespace proc:
 
     proc := name
     if background:
-      proc = "setsid " + proc + " &"
+      proc = "setsid " + proc
 
-    if system(proc.c_str()) != 0:
-      pass
+    auto *th = new thread([=]() {
+      c_str := proc.c_str()
+      _ := system(c_str)
 
+      debug "PROGRAM EXITED"
+    })

src/proc/main.cpy
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]):
     args.push_back(argv[i])
 
   if subcmd == "ls":
-    procs := proc::ls(args)
+    procs := proc::list_procs(args)
     mem_usage := proc::collect_mem(procs)
     for auto p : procs:
       print p.pid, p.cmdline, mem_usage[p.pid]

src/remux/apps.cpy
@@ -148,7 +148,7 @@ class AppReader:
       app.is_running = false
       bins.push_back(app.bin)
 
-    procs := proc::ls(bins)
+    procs := proc::list_procs(bins)
     is_running := proc::is_running(bins, procs)
 
     for auto &app : self.apps:

commit ebff20e9ca100764a947400c104e9274c6ed44bc

[remux] return to unthreaded way of launching applications
there is a bug where users can not kill xochitl through remux and
restart it. it shows up in various forms and seems to be related to
the way of launching the applications.

by using '&' (instead of running the program in just a separate thread),
this bug goes away.

(bug was introduced in 154af55c2d40a371e6d9447070976b937fc3a5eb)

src/shared/proc.cpy
@@ -336,11 +336,7 @@ namespace proc:
 
     proc := name
     if background:
-      proc = "setsid " + proc
+      proc = "setsid " + proc + " &"
 
-    auto *th = new thread([=]() {
-      c_str := proc.c_str()
-      _ := system(c_str)
+    _ := system(proc.c_str())
 
-      debug "PROGRAM EXITED"
-    })